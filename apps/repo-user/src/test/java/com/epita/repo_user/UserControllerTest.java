02ffcc7d8888138864000ed24d1e2b1db5a3a23a
Samy YACEF <samy.yacef@epita.fr>
2025-03-27 23:42:05 +0100

    format(repo-user): fix format

--- apps/repo-user/src/test/java/com/epita/repo_user/UserControllerTest.java
+++ apps/repo-user/src/test/java/com/epita/repo_user/UserControllerTest.java
@@ -1,10 +1,11 @@
-package com.epita.repo_user.controller;
+package com.epita.repo_user;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.notNullValue;
 
 import com.epita.exchange.auth.service.AuthContext;
+import com.epita.repo_user.controller.RepoUserController;
 import com.epita.repo_user.controller.request.CreateUserRequest;
 import com.epita.repo_user.controller.request.LoginRequest;
 import com.epita.repo_user.controller.request.ModifyUserRequest;
@@ -15,7 +16,6 @@ import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import jakarta.inject.Inject;
 import java.io.File;
-import java.io.File;
 import org.junit.jupiter.api.*;
 
 @QuarkusTest
@@ -24,32 +24,7 @@ import org.junit.jupiter.api.*;
 public class UserControllerTest {
 
   @Inject UserService userService;
-  @Inject UserService userService;
 
-  // @Inject
-  // S3Service s3Service;
-
-  // @Inject
-  // RedisPublisher redisPublisher;
-
-  @Inject AuthContext authContext;
-
-  private static String userId;
-  private static final String TEST_USERNAME = "testuser";
-  private static final String TEST_PASSWORD = "Password123!";
-  private static final String TEST_EMAIL = "test@example.com";
-
-  @Test
-  @Order(1)
-  @DisplayName("Should create a new user successfully")
-  public void testCreateUser() {
-    CreateUserRequest request = new CreateUserRequest();
-    request.username = TEST_USERNAME;
-    request.password = TEST_PASSWORD;
-    request.email = TEST_EMAIL;
-
-    Response response =
-        given()
   // @Inject
   // S3Service s3Service;
 
@@ -83,103 +58,6 @@ public class UserControllerTest {
             .body("username", equalTo(TEST_USERNAME))
             .body("email", equalTo(TEST_EMAIL))
             .body("id", notNullValue())
-            .extract()
-            .response();
-
-    userId = response.jsonPath().getString("id");
-  }
-
-  @Test
-  @Order(2)
-  @DisplayName("Should login successfully")
-  public void testLogin() {
-    LoginRequest request = new LoginRequest();
-    request.username = TEST_USERNAME;
-    request.password = TEST_PASSWORD;
-
-    given()
-        .contentType(ContentType.JSON)
-        .body(request)
-        .when()
-        .post("/login")
-        .then()
-        .statusCode(200)
-        .body("username", equalTo(TEST_USERNAME))
-        .body("id", notNullValue());
-  }
-
-  @Test
-  @Order(3)
-  @DisplayName("Should get current user successfully")
-  public void testGetCurrentUser() {
-    given()
-        .when()
-        .get("/user")
-        .then()
-        .statusCode(200)
-        .body("username", equalTo(TEST_USERNAME))
-        .body("email", equalTo(TEST_EMAIL));
-  }
-
-  @Test
-  @Order(4)
-  @DisplayName("Should get user by ID successfully")
-  public void testGetUserById() {
-    given()
-        .pathParam("id", userId)
-        .when()
-        .get("/user/{id}")
-        .then()
-        .statusCode(200)
-        .body("username", equalTo(TEST_USERNAME));
-  }
-
-  @Test
-  @Order(5)
-  @DisplayName("Should update user profile successfully")
-  public void testUpdateUser() {
-    ModifyUserRequest request = new ModifyUserRequest();
-    request.bio = "This is my updated bio";
-    request.username = "updateduser";
-
-    given()
-        .contentType(ContentType.JSON)
-        .body(request)
-        .when()
-        .put("/user")
-        .then()
-        .statusCode(200)
-        .body("bio", equalTo("This is my updated bio"))
-        .body("username", equalTo("updateduser"));
-  }
-
-  @Test
-  @Order(6)
-  @DisplayName("Should upload profile image successfully")
-  public void testUploadProfileImage() throws Exception {
-    File testFile = File.createTempFile("profile", ".jpg");
-    testFile.deleteOnExit();
-
-    given()
-        .contentType("multipart/form-data")
-        .multiPart("file", testFile, "image/jpeg")
-        .when()
-        .put("/user/image")
-        .then()
-        .statusCode(200)
-        .body("profileImage", notNullValue());
-  }
-
-  @Test
-  @Order(7)
-  @DisplayName("Should delete user successfully")
-  public void testDeleteUser() {
-    given().when().delete("/user").then().statusCode(204);
-
-    given().pathParam("id", userId).when().get("/user/{id}").then().statusCode(404);
-  }
-
-  // Additional tests for error cases...
             .extract()
             .response();
 
