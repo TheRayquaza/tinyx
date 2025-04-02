171a03bcf36dd95a6823fbc656ce7afd652c748a
Gabriel Toledano <gabriel.toledano@epita.fr>
2025-04-03 00:01:58 +0200

    feat(srvc-user-timeline) added converter & sort post in service

--- apps/srvc-user-timeline/src/main/java/com/epita/srvc_user_timeline/service/UserTimelineService.java
+++ apps/srvc-user-timeline/src/main/java/com/epita/srvc_user_timeline/service/UserTimelineService.java
@@ -1,19 +1,12 @@
 package com.epita.srvc_user_timeline.service;
 
-import com.epita.exchange.redis.aggregate.PostAggregate;
-import com.epita.exchange.redis.aggregate.UserAggregate;
-import com.epita.exchange.redis.command.BlockCommand;
-import com.epita.exchange.redis.command.FollowCommand;
-import com.epita.exchange.redis.command.LikeCommand;
 import com.epita.srvc_user_timeline.repository.UserTimelineRepository;
 import com.epita.srvc_user_timeline.service.entity.UserTimelineEntity;
 import com.epita.srvc_user_timeline.service.entity.UserTimelinePostEntity;
-import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import java.util.List;
 
-@ApplicationScoped
-public class UserTimelineService {
+class UserTimelineService {
   @Inject UserTimelineRepository userTimelineRepository;
 
   private List<UserTimelinePostEntity> sortPosts(List<UserTimelinePostEntity> posts) {
@@ -33,28 +26,8 @@ public class UserTimelineService {
     return posts;
   }
 
-  public UserTimelineEntity getUserTimeline(String userId) {
+  UserTimelineEntity getUserTimeline(String userId) {
     // TODO use repository to fetch all posts useful for this user
     return new UserTimelineEntity();
   }
-
-  public void handlePostAggregate(PostAggregate postAggregate){
-      // TODO
-  }
-
-    public void handleUserAggregate(UserAggregate userAggregate) {
-        // TODO
-    }
-
-    public void handleFollowCommand(FollowCommand followCommand) {
-        // TODO
-    }
-
-    public void handleLikeCommand(LikeCommand likeCommand) {
-        // TODO
-    }
-
-    public void handleBlockCommand(BlockCommand blockCommand) {
-        // TODO
-    }
 }
