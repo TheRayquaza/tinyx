93918f18882b015ca18db0b972e627371c33b2d4
ap <alexis.pinson@epita.fr>
2025-04-02 17:14:34 +0200

    feat(srvc-user-timeline): added more files and started to create controller

--- apps/srvc-user-timeline/src/main/java/com/epita/srvc_user_timeline/controller/UserTimelineController.java
+++ apps/srvc-user-timeline/src/main/java/com/epita/srvc_user_timeline/controller/UserTimelineController.java
@@ -1,28 +1,15 @@
 package com.epita.srvc_user_timeline.controller;
 
-import com.epita.srvc_user_timeline.controller.contract.UserTimelineResponse;
-import com.epita.srvc_user_timeline.converter.UserTimelineEntityToUserTimelineResponse;
-import com.epita.srvc_user_timeline.service.UserTimelineService;
-import io.quarkus.security.Authenticated;
-import jakarta.inject.Inject;
-import jakarta.ws.rs.*;
-import jakarta.ws.rs.core.MediaType;
-
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 @Path("/")
-public class UserTimelineController implements UserTimelineControllerAPI {
-
-  @Inject UserTimelineService userTimelineService;
-  @Inject UserTimelineEntityToUserTimelineResponse userTimelineEntityToUserTimelineResponse;
-
-  @GET
-  @Path("/user-timeline/{id}")
-  @Authenticated
-  @Override
-  public UserTimelineResponse getUserTimeline(@PathParam("id") String id) {
-    logger().info("GET /user-timeline/{}", id);
-    return userTimelineEntityToUserTimelineResponse.convertNotNull(
-        userTimelineService.getUserTimeline(id));
-  }
+public class UserTimelineController implements UserTimelineController {
+    @GET
+    @Path("/user-timeline/{id}")
+    @Authenticated
+    @Override
+    public UserEntity getUserTimeline(@PathParam("id") String id) {
+        logger().info("GET /user-timeline/{}", id);
+        return userService.getUser(new ObjectId(id));
+    }
 }
--- apps/srvc-user-timeline/src/main/java/com/epita/srvc_user_timeline/controller/UserTimelineControllerAPI.java
+++ apps/srvc-user-timeline/src/main/java/com/epita/srvc_user_timeline/controller/UserTimelineControllerAPI.java
@@ -1,31 +1,20 @@
 package com.epita.srvc_user_timeline.controller;
 
-import com.epita.exchange.utils.Logger;
-import com.epita.srvc_user_timeline.UserTimelineErrorCode;
-import com.epita.srvc_user_timeline.controller.contract.UserTimelineResponse;
-import jakarta.ws.rs.*;
-import jakarta.ws.rs.core.MediaType;
-import org.eclipse.microprofile.openapi.annotations.Operation;
-import org.eclipse.microprofile.openapi.annotations.media.Content;
-import org.eclipse.microprofile.openapi.annotations.media.Schema;
-import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
-import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
-
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 @Path("/")
 public interface UserTimelineControllerAPI extends Logger {
-  @Path("/user-timeline/{id}")
-  @Operation(summary = "Get a specific user's account timeline")
-  @APIResponses({
-    @APIResponse(
-        responseCode = "200",
-        description = "User account timeline retrieved successfully",
-        content = @Content(schema = @Schema(implementation = UserTimelineResponse.class))),
-    @APIResponse(
-        responseCode = "404",
-        description = "User not found",
-        content = @Content(schema = @Schema(implementation = UserTimelineErrorCode.class)))
-  })
-  UserTimelineResponse getUserTimeline(@PathParam("id") String id);
+    @Path("/user-timeline/{id}")
+    @Operation(summary = "Get a specific user's account timeline")
+    @APIResponses({
+            @APIResponse(
+                    responseCode = "200",
+                    description = "User account timeline retrieved successfully",
+                    content = @Content(schema = @Schema(implementation = UserEntity.class))),
+            @APIResponse(
+                    responseCode = "404",
+                    description = "User not found",
+                    content = @Content(schema = @Schema(implementation = UserTimelineErrorCode.class)))
+    })
+    UserEntity getUserAccount(@PathParam("id") String id);
 }
