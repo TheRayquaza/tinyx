b3355076f87315fc5925946e3788c24d8b06bf21
Gabriel Toledano <gabriel.toledano@epita.fr>
2025-04-03 00:01:58 +0200

    feat(srvc-user-timeline) added converter & sort post in service

---
+++ apps/srvc-user-timeline/src/main/java/com/epita/srvc_user_timeline/converter/UserTimelinePostEntityToUserTimelinePostResponse.java
@@ -0,0 +1,24 @@
+package com.epita.srvc_user_timeline.converter;
+
+import com.epita.srvc_user_timeline.controller.contract.UserTimelinePostResponse;
+import com.epita.srvc_user_timeline.service.entity.UserTimelinePostEntity;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class UserTimelinePostEntityToUserTimelinePostResponse {
+  public UserTimelinePostResponse convertNotNull(UserTimelinePostEntity input) {
+    return new UserTimelinePostResponse()
+        .withId(input.getId())
+        .withUserId(input.getUserId())
+        .withText(input.getText())
+        .withMedia(input.getMedia())
+        .withDeleted(input.isDeleted())
+        .withCreatedAt(input.getCreatedAt())
+        .withUpdatedAt(input.getUpdatedAt())
+        .withOwnerId(input.getOwnerId())
+        .withLikedAt(input.getLikedAt())
+        .withIsReply(input.getIsReply())
+        .withReplyToPostId(input.getReplyToPostId())
+        .withRepostId(input.getRepostId());
+  }
+}
