8d9be22884f983a9fffbeb0005cacc8ff86bf6a7
Mateo Lelong <mateo.lelong@epita.fr>
2025-04-05 22:46:46 +0200

    feat(k8s): make mongo shard work with repo user

--- k8s/mongodb/mongo-configs/init.js
+++ k8s/mongodb/mongo-configs/init.js
@@ -4,7 +4,7 @@ rs.initiate(
         configsvr: true,
         members: [
             { _id: 0, host: "mongo-config-0.mongo-config.mongo.svc.cluster.local:27019" },
-            { _id: 1, host: "mongo-config-1.mongo-config.mongo.svc.cluster.local:27019" },
+            //{ _id: 1, host: "mongo-config-1.mongo-config.mongo.svc.cluster.local:27019" },
             //{ _id: 0, host: "mongo-config-2.mongo-config.mongo.svc.cluster.local:27019" },
         ]
     }
