4bfb247fe81ee91c102061adbdde28b2b1c3bfb0
Mateo Lelong <mateo.lelong@epita.fr>
2025-04-10 19:43:46 +0200

    fix: test to new spec

--- apps/srvc-search/src/main/java/com/epita/srvc_search/controller/SrvcSearchController.java
+++ apps/srvc-search/src/main/java/com/epita/srvc_search/controller/SrvcSearchController.java
@@ -9,7 +9,7 @@ import jakarta.inject.Inject;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.POST;
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
